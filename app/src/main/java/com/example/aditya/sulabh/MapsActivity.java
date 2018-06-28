1dd3e3d15689cf2e4bef6334cc937410576e64ef
20Aditya <aditya20chandel@gmail.com>
2018-06-28 15:13:25 +0530

    Star Rating feature added

--- app/src/main/java/com/example/aditya/sulabh/MapsActivity.java
+++ app/src/main/java/com/example/aditya/sulabh/MapsActivity.java
@@ -33,6 +33,7 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.FirebaseApp;
 
 import java.util.Map;
 
@@ -53,6 +54,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+
 
 
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
--- app/src/main/java/com/example/aditya/sulabh/PlacesDetailsActivity.java
+++ app/src/main/java/com/example/aditya/sulabh/PlacesDetailsActivity.java
@@ -4,12 +4,27 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.firebase.client.Firebase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
+
 public class PlacesDetailsActivity extends AppCompatActivity {
+
+
+    EditText editText,editText1;
+    RatingBar ratingBar,ratingBar2;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +32,8 @@ public class PlacesDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_places_details);
 
 
-        String index = getIntent().getStringExtra(Utilities.Placeindex);
+
+        final String index = getIntent().getStringExtra(Utilities.Placeindex);
         ImageView image = (ImageView) findViewById(R.id.image);
         TextView textView2 = (TextView)findViewById(R.id.textView2);
 
@@ -30,12 +46,61 @@ public class PlacesDetailsActivity extends AppCompatActivity {
         else
             image.setImageResource(R.drawable.notavailable);
 
-        textView2.setText(String.valueOf(placelocation.getLatitude()));
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        editText = (EditText)findViewById(R.id.editText);
+        editText1 = (EditText)findViewById(R.id.editText2);
+        Button btn = (Button)findViewById(R.id.submit);
+        ratingBar = (RatingBar)findViewById(R.id.ratingBar2);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String vicinity = PlacesListActivity.vicinity.get(Integer.parseInt(index));
+                if(editText1.getText().toString().trim().isEmpty())
+                    editText1.setError("This field is required");
+                else if(ratingBar.getRating()==0)
+                    Toast.makeText(PlacesDetailsActivity.this,"You haven't rated",Toast.LENGTH_LONG).show();
+                else
+                {
+                    String email = Encode(editText1.getText().toString().trim());
+                    Log.d("Mail","Email = "+ email);
+                    databaseReference.child(vicinity).child(email).child("Rating").setValue(ratingBar.getRating());
+
+                    if(editText.getText().toString().trim().isEmpty())
+                        databaseReference.child(vicinity).child(email).child("Comments").setValue("No comments available");
+                    else
+                        databaseReference.child(vicinity).child(email).child("Comments").setValue(editText.getText().toString().trim());
+
+
+                }
+
+                editText.getText().clear();
+                editText1.getText().clear();
+                ratingBar.setRating(0);
+            }
+        });
+
+
+        ratingBar2 = (RatingBar)findViewById(R.id.ratingBar);
+
+
+
 
     }
 
+    public String Encode(String string)
+    {
+        string = string.replace(".","@");
+        string = string.replace("#","@");
+        string = string.replace("$","@");
+        string = string.replace("[","@");
+        string = string.replace("]","@");
 
-    public void getphotoURL(){
-
+        return string;
     }
+
+
+
 }
